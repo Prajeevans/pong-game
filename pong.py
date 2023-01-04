86cfd7a025af43077e73d28e0448478c07fa21b5
Prajeevan <prajee.bajee@gmail.com>
2023-01-04 19:50:49 +0530

    feat: add paddle and score logic

--- pong.py
+++ pong.py
@@ -1,10 +1,25 @@
 import turtle 
 
+
+score_a = 0
+score_b = 0
+
+
 win = turtle.Screen()
 win.setup(800,600)
 win.bgcolor("blue")
 win.title("Pong-Game")
 win.tracer(0)
+
+# Score board
+pen = turtle.Turtle()
+pen.speed()
+pen.color("green")
+pen.penup()
+pen.goto(0,260)
+pen.write(f"Player A: {score_a} Player B: {score_b}", align="Center", font=["ariel", 24, 'bold'])
+pen.hideturtle()
+
 
 # left paddle
 left_paddle = turtle.Turtle()
@@ -30,7 +45,7 @@ ball.speed(0)
 ball.shape("circle")
 ball.color("white")
 ball.penup()
-ball.dx = .5
+ball.dx = .4
 ball.dy = .5
 
 #Logic for Left paddle
@@ -50,8 +65,8 @@ def right_paddle_down():
     right_paddle.sety(right_paddle.ycor()- 20)
 
 win.listen()
-# win.onkeypress(right_paddle_up, "Up")
-# win.onkeypress(right_paddle_down, "Down")
+win.onkeypress(right_paddle_up, "i")
+win.onkeypress(right_paddle_down, "k")
 
 
 while True:
@@ -60,6 +75,33 @@ while True:
     ball.setx(ball.xcor() + ball.dx)
     ball.sety(ball.ycor()+ ball.dy)
     if ball.ycor()>=290:
+        ball.sety(290)
         ball.dy *= -1
+        
     elif ball.ycor()<=-290:
+        ball.sety(-290)
         ball.dy *= -1
+
+    # right and left wall
+    if ball.xcor()>=380:
+        ball.setx(380)
+        ball.dx *= -1
+        score_a += 1
+        pen.clear()
+        pen.write(f"Player A: {score_a} Player B: {score_b}", align="Center", font=["ariel", 24, 'bold'])
+
+    elif ball.xcor()<=-380:
+        ball.setx(-380)
+        ball.dx *= -1
+        score_b += 1
+        pen.clear()
+        pen.write(f"Player A: {score_a} Player B: {score_b}", align="Center", font=["ariel", 24, 'bold'])
+
+    
+    # paddle hit 
+    if ball.xcor()> 370 and right_paddle.ycor() - 50< ball.ycor() < right_paddle.ycor()+50 :
+        ball.setx(370)
+        ball.dx *= -1
+    if ball.xcor()<-370 and left_paddle.ycor() - 50< ball.ycor() < left_paddle.ycor()+50 :
+        ball.setx(-370)
+        ball.dx *= -1
